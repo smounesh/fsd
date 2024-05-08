8e70e9b12b9905b202bc7cca9905b4426019e4ef
Shankar Mounesh <92090462+smounesh@users.noreply.github.com>
2024-05-08 17:33:53 +0530

    final commit

--- day19/ans.sql
+++ day19/ans.sql
@@ -23,28 +23,17 @@ EXEC GetBooksByAuthor Johnson
 
 -- 2) Create a stored procedure that will take the employee's firstname and print all the titles sold by him/her, price, quantity, and the cost
 
+create proc GetBooksDetailsByEmployee
+@firstname varchar(20)
+as begin
+select title, price, qty, (qty * price) as cost
+from titles t
+join sales s on s.title_id = t.title_id
+join employee e on e.pub_id = t.pub_id
+where fname = @firstname
+end
 
-CREATE PROCEDURE GetTitleSoldByEmployee
-    @EmployeeFirstName NVARCHAR(50)
-AS
-BEGIN
-    SELECT
-        t.title AS TitleName,
-        t.price AS Price,
-        s.qty AS Quantity,
-        (t.price * s.qty) AS Cost
-    FROM
-        sales s
-    JOIN
-        titles t ON s.title_id = t.title_id
-    JOIN
-        employee e ON s.stor_id = e.emp_id
-    WHERE
-        e.fname = @EmployeeFirstName
-END
-
-
-EXEC GetTitleSoldByEmployee Paolo;
+EXEC GetBooksDetailsByEmployee Paolo;
 
 -- 3) Create a query that will print all names from authors and employees
 
@@ -62,24 +51,12 @@ FROM
 
 -- 4) Create a query that will fetch data from sales, titles, publisher, and authors table to print title name, Publisher's name, author's full name with quantity ordered and price for the order for all orders, and print the first 5 orders after sorting them based on the price of the order
 
-SELECT TOP 5
-    t.title AS TitleName,
-    p.pub_name AS PublisherName,
-    CONCAT(a.au_fname, ' ', a.au_lname) AS AuthorFullName,
-    s.qty AS QuantityOrdered,
-    t.price AS OrderPrice
-FROM 
-    sales s
-JOIN 
-    titles t ON s.title_id = t.title_id
-JOIN 
-    publishers p ON t.pub_id = p.pub_id
-JOIN 
-    titleauthor ta ON t.title_id = ta.title_id
-JOIN 
-    authors a ON ta.au_id = a.au_id
-ORDER BY 
-    t.price;
+select top 5 title, pub_name, concat(a.au_fname,' ', a.au_lname) as [Author Name], qty, (qty * price) as cost from titles t
+join publishers p on t.pub_id = p.pub_id
+join titleauthor ta on ta.title_id = t.title_id
+join authors a on a.au_id = ta.au_id
+join sales s on s.title_id = t.title_id
+order by cost
 
 
 -- 5) Grant SELECT permission on the Titles table to user1
